abc87f61180acf47f502f13e58df2deef8c7d003
Miguel Ángel Ávila Rosas <148963870+MiguelAngelAvilaRosas1DAW@users.noreply.github.com>
2023-12-14 20:33:58 +0100

    Add files via upload

---
+++ CRBV/JS/CRBV.js
@@ -0,0 +1,36 @@
+/********************************************/
+/* JavaScript for authors                   */
+/* Author: Miguel Ángel Ávila Rosas         */
+/* Version: 2.0                             */
+/* Created on: October 29, 2023             */
+/* Updated on: December 14, 2023            */
+/********************************************/
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
