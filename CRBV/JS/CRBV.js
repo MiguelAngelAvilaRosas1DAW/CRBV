70633cb0ef33b9ca36d22d9b118c1ba2e0adbd06
Miguel Ángel Ávila Rosas <148963870+MiguelAngelAvilaRosas1DAW@users.noreply.github.com>
2023-12-25 17:26:08 +0100

    UPDATE V2.0
    A new has been release! Go play it now!
    Includes:
    ·New BF and Spooky icons.
    ·4 new EXTRA songs!

---
+++ CRBV/JS/CRBV.js
@@ -0,0 +1,48 @@
+/********************************************/
+/* JavaScript for authors                   */
+/* Author: Miguel Ángel Ávila Rosas         */
+/* Version: 2.0                             */
+/* Created on: October 29, 2023             */
+/* Updated on: December 14, 2023            */
+/********************************************/
+
+/*********************TOP NAV*********************/
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function showSections() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+/*********************GALLERY*********************/
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
